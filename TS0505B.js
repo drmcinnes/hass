c1477106c1d88cf35ecdc415ada2e0fe63638acf
Darren McInnes <dmcinnes.git@archer.com.au>
2022-01-25 15:19:42 +1100

    Add files via upload
    Mercator Ikuu 90mm RGBW Downlight

---
+++ TS0505B.js
@@ -0,0 +1,26 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+    fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3000_xr5m6kfg'}],
+    zigbeeModel: ['TS0505B'],
+    model: 'SMD4109W-RGB-ZB',
+    vendor: 'Mercator',
+    description: 'Mercator Ikuu 90mm Downlight RGBW',
+    // Note that fromZigbee, toZigbee and exposes are missing here since we use extend here.
+    // Extend contains a default set of fromZigbee/toZigbee converters and expose for common device types.
+    // The following extends are available:
+    // - extend.switch
+    // - extend.light_onoff_brightness
+    // - extend.light_onoff_brightness_colortemp
+    // - extend.light_onoff_brightness_color
+    // - extend.light_onoff_brightness_colortemp_color
+    extend: extend.light_onoff_brightness_colortemp_color(),
+};
+
+module.exports = definition;
