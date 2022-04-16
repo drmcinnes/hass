8053ce389f8edbd5dab3957c40a6b31e742a40ee
Darren McInnes <dmcinnes.git@archer.com.au>
2022-04-16 15:25:51 +1000

    Add files via upload

---
+++ TS0502B.js
@@ -0,0 +1,26 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+    fingerprint: [{modelID: 'TS0502B', manufacturerName: '_TZ3000_armwcncd'}],
+    zigbeeModel: ['TS0502B'],
+    model: 'SMD4109W-ZB',
+    vendor: 'Mercator',
+    description: 'Mercator Ikuu 90mm Downlight White',
+    // Note that fromZigbee, toZigbee and exposes are missing here since we use extend here.
+    // Extend contains a default set of fromZigbee/toZigbee converters and expose for common device types.
+    // The following extends are available:
+    // - extend.switch
+    // - extend.light_onoff_brightness
+    // - extend.light_onoff_brightness_colortemp
+    // - extend.light_onoff_brightness_color
+    // - extend.light_onoff_brightness_colortemp_color
+    extend: extend.light_onoff_brightness_colortemp(),
+};
+
+module.exports = definition;
